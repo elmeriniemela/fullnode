837b050e4fccf1d84cd204b9e3fc2ecdacffc15d
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-10-01 11:25:52 +0300

    add lnd pw file

--- lnd.sh
+++ lnd.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=bitcoin-blockchain-datadir --bitcoin.node=bitcoind $@
+if [ ! -f "bitcoin-blockchain-datadir/lnd-pw.txt" ]; then
+    ./go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=bitcoin-blockchain-datadir --bitcoin.node=bitcoind $@
+else
+    ./go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=bitcoin-blockchain-datadir --bitcoin.node=bitcoind --wallet-unlock-password-file=bitcoin-blockchain-datadir/lnd-pw.txt $@
+fi
+
