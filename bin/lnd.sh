72bd390413eb3d06c2b00c0bb3fb3de5a64040fc
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-05-19 20:38:52 +0300

    fix lnd

--- bin/lnd.sh
+++ bin/lnd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f "config/lnd-pw.txt" ]; then
-    ./fullnode/go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=data --bitcoind.config=../config/bitcoin.conf --bitcoin.node=bitcoind $@
+    ./fullnode/go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=data --bitcoind.config=config/bitcoin.conf --bitcoin.node=bitcoind $@
 else
-    ./fullnode/go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=data --bitcoind.config=../config/bitcoin.conf --bitcoin.node=bitcoind --wallet-unlock-password-file=config/lnd-pw.txt $@
+    ./fullnode/go/bin/lnd --bitcoin.active --bitcoin.mainnet --bitcoind.dir=data --bitcoind.config=config/bitcoin.conf --bitcoin.node=bitcoind --wallet-unlock-password-file=config/lnd-pw.txt $@
 fi
 
