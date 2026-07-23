0477fbc0c5ed008e0deaaea9bb8cbc7d8d143d3b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-07-23 18:35:27 +0300

    update configs

--- bin/bitcoinknotsd.sh
+++ bin/bitcoinknotsd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./fullnode/bitcoinknots/build/bin/bitcoind -datadir=knotsdata -conf=../config/knots.conf $@
+./fullnode/bitcoinknots/build/bin/bitcoind -datadir=knotsdata -conf=../config/knots.conf $@ -pid=/run/bitcoinknotsd/bitcoinknotsd.pid -startupnotify='systemd-notify --ready' -shutdownnotify='systemd-notify --stopping'
